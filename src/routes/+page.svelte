3599fbb664b30ab31f980eefea0c53e9eb5e6b88
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-02-15 19:40:40 +0100

    master

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2">THIS IS Another PULL REQ TEST.</h2>
+		<h2 class="h2">THIS IS TOTALLY CORRECT, THANK YOU FOR BEEING HERE.</h2>
 		<!-- Animated Logo -->
 		<figure>
 			<section class="img-bg" />
